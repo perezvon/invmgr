5b69c1aa5f2994ba83565d8917ffb25e39e16ffc
John A Maloney <johnamaloney@Johns-MacBook-Pro-2.local>
2017-09-08 13:32:45 -0500

    mlab mongoose connect

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const router = require('./router')
 const app = express();
 
 //set our port to either a predetermined port number if you have set it up, or 3001
-const port = process.env.API_PORT || 3001;
+const port = process.env.PORT || 3001;
 
 const uristring =
     process.env.MONGODB_URI ||
